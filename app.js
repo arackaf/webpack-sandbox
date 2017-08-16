9b13fbf6560ae9ff143365538b4b435b687810cd
Adam Rackis <adamrackis@gmail.com>
2017-08-16 08:59:30 -0500

    committing async attempts

--- app.js
+++ app.js
@@ -11,21 +11,21 @@ if (+new Date() < 0){  //it won't be
 } else {
     debugger;
     setTimeout(() => {
-        __webpack_require__(moduleId).then(({default: res}) => {
+        __webpack_require__.e/* import() */(moduleId).then(() => __webpack_require__(moduleId).then(({default: res}) => {
             debugger
-        });
+        }));
     });
 
     setTimeout(() => {
-        __webpack_require__(moduleId2).then(({default: res}) => {
+        __webpack_require__.e/* import() */(moduleId2).then(() => __webpack_require__(moduleId2).then(({default: res}) => {
             debugger
-        });
+        }));
     });    
 
     setTimeout(() => {
-        __webpack_require__(moduleId3).then(({default: res}) => {
+        __webpack_require__.e/* import() */(moduleId3).then(() => __webpack_require__(moduleId3).then(({default: res}) => {
             debugger
-        });    
+        }));    
     });
     // __webpack_require__.e/* import() */(0).then(() => __webpack_require__(1)).then(({default: obj}) => {
     //     console.log(obj);
